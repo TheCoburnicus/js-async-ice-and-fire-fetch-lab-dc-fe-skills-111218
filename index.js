9b7d5878ad4ab87e1cb4c765da19305de87f63e2
Nathan Coburn <nathanlcoburn@gmail.com>
2019-01-10 01:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
   //Find in Browser Console--All Houses in GoT; 5th book in series; 1031st character in series//
-}
-
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
@@ -31,9 +20,21 @@ fetch('https://anapioficeandfire.com/api/books/5')
   .then(json => renderBooks(json));
   
 document.addEventListener('DOMContentLoaded', function() {
-  renderBooks()
+  fetchBooks()
 })
-fetch('https://anapioficeandfire.com/api/books/5')
+fetch('https://anapioficeandfire.com/api/characters/1031')
   .then(resp => resp.json())
-  .then(json => renderBooks(json));
+  .then(json => fetchBooks(json));
   
+  
+}
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+
